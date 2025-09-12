33c58ee7c3fcedc7a2716a0519fe28b3b0cf876a
kamado-spro <jnnguy+kamadospro@gmail.com>
2025-09-12 12:35:19 -0400

    Add files via upload

--- script.js
+++ script.js
@@ -47,6 +47,60 @@ const palette = [
     { name: "Dark magenta", hex: "#4d192c" }
 ];
 
+/*
+Color definitions are from 
+https://discord.com/channels/890339612441063494/1284530105816711168/1375040718308900875 
+
+------ COLOUR DEFINITION ----------- COLOUR HEX ------------- COLOUR NAME ------
+#define COLOR_IUIUIU_ORANGE          lv_color_hex(0xE64829) // Gaggiuino Orange 
+#define COLOR_IUIUIU_BLUE            lv_color_hex(0x023047) // Deep Blue 
+#define COLOR_IUIUIU_RED             lv_color_hex(0xd00000) // True Red 
+#define COLOR_IUIUIU_DIRTY_GREEN     lv_color_hex(0x84a98c) // Dirty Green 
+#define COLOR_IUIUIU_LIGHT_BROWN     lv_color_hex(0xe6ccb2) // Light Brown 
+#define COLOR_IUIUIU_PINKISH         lv_color_hex(0xffb5a7) // Pinkish 
+#define COLOR_IUIUIU_DEEP_PURP       lv_color_hex(0xb5179e) // Deep Purple 
+#define COLOR_ESPRESSO               lv_color_hex(0x4B382A) // Espresso Brown 
+#define COLOR_LATTE                  lv_color_hex(0xC89F91) // Latte Cream 
+#define COLOR_MOCHA                  lv_color_hex(0x3E2723) // Mocha Brown 
+#define COLOR_CARAMEL                lv_color_hex(0xD2691E) // Caramel Drizzle 
+#define COLOR_MACCHIATO              lv_color_hex(0xFFD700) // Macchiato Gold 
+#define COLOR_CAFFE_AU_LAIT          lv_color_hex(0xA67B5B) // Caffé au Lait 
+#define COLOR_CAPPUCCINO             lv_color_hex(0xB5651D) // Cappuccino Foam 
+#define COLOR_AMERICANO              lv_color_hex(0x2F1B0C) // Americano Black 
+#define COLOR_IRISH_COFFEE           lv_color_hex(0x5A3A22) // Irish Coffee 
+#define COLOR_VANILLA_BEAN           lv_color_hex(0xF5E6CC) // Vanilla Bean 
+#define COLOR_ICED_COFFEE            lv_color_hex(0x8B6F47) // Iced Coffee 
+#define COLOR_RISTRETTO              lv_color_hex(0x362511) // Ristretto 
+#define COLOR_LIGHT_BLUE             lv_color_hex(0xADD8E6) // Light Blue 
+#define COLOR_MEDIUM_LIGHT_BLUE      lv_color_hex(0x87CEFA) // Medium Light Blue 
+#define COLOR_DARK_BLUE              lv_color_hex(0x00008B) // Dark Blue 
+#define COLOR_LIGHT_GREEN            lv_color_hex(0x90EE90) // Light Green 
+#define COLOR_MEDIUM_LIGHT_GREEN     lv_color_hex(0x32CD32) // Medium Light Green 
+#define COLOR_DARK_GREEN             lv_color_hex(0x006400) // Dark Green 
+#define COLOR_LIGHT_YELLOW           lv_color_hex(0xFFFFE0) // Light Yellow 
+#define COLOR_MEDIUM_LIGHT_YELLOW    lv_color_hex(0xFFFACD) // Medium Light Yellow 
+#define COLOR_DARK_YELLOW            lv_color_hex(0xFFD700) // Dark Yellow 
+#define COLOR_LIGHT_PURPLE           lv_color_hex(0xE6E6FA) // Light Purple 
+#define COLOR_MEDIUM_LIGHT_PURPLE    lv_color_hex(0x9370DB) // Medium Light Purple 
+#define COLOR_DARK_PURPLE            lv_color_hex(0x4B0082) // Dark Purple 
+#define COLOR_LIGHT_RED              lv_color_hex(0xFFB6C1) // Light Red 
+#define COLOR_MEDIUM_LIGHT_RED       lv_color_hex(0xFF6347) // Medium Light Red 
+#define COLOR_DARK_RED               lv_color_hex(0x8B0000) // Dark Red 
+#define COLOR_LIGHT_BROWN            lv_color_hex(0xD2B48C) // Light Brown 
+#define COLOR_MEDIUM_LIGHT_BROWN     lv_color_hex(0xA0522D) // Medium Light Brown 
+#define COLOR_DARK_BROWN             lv_color_hex(0x654321) // Dark Brown 
+#define COLOR_LIGHT_ORANGE           lv_color_hex(0xFFA07A) // Light Orange 
+#define COLOR_MEDIUM_LIGHT_ORANGE    lv_color_hex(0xFF8C00) // Medium Light Orange 
+#define COLOR_DARK_ORANGE            lv_color_hex(0xFF4500) // Dark Orange 
+#define COLOR_LIGHT_PINK             lv_color_hex(0xFFB6C1) // Light Pink 
+#define COLOR_MEDIUM_LIGHT_PINK      lv_color_hex(0xFF69B4) // Medium Light Pink 
+#define COLOR_DARK_PINK              lv_color_hex(0xC71585) // Dark Pin 
+#define COLOR_IUIUIU_PASTEL_AQUA     lv_color_hex(0x8cc1d6) // Light Aqua 
+#define COLOR_IUIUIU_DARK_TEAL       lv_color_hex(0x073e55) // Dark Teal 
+#define COLOR_IUIUIU_DARK_MAGENTA    lv_color_hex(0x4d192c) // Dark magenta 
+*/
+
+
 window.addEventListener('DOMContentLoaded', () => {
     handlePrimaryColor(palette[0]);
     handleSecondaryColor(palette[1]);
@@ -61,6 +115,15 @@ document.querySelectorAll('.sidebar-btn').forEach(button => {
         button.classList.add('active');
     });
 });
+
+// Sidebar button interactions
+document.querySelectorAll('.profile-btn').forEach(button => {
+    button.addEventListener('click', () => {
+        document.querySelectorAll('.profile-btn').forEach(b => b.classList.remove('active'));
+        button.classList.add('active');
+    });
+});
+
 
 // Control button interactions
 document.querySelectorAll('.control-button').forEach(button => {
@@ -77,9 +140,9 @@ document.querySelectorAll('.weight-btn').forEach(btn => {
         let currentValue = parseFloat(valueEl.textContent);
 
         if (btn.textContent === '+') {
-            currentValue += 0.5;
+            currentValue += 1.0;
         } else {
-            currentValue = Math.max(0, currentValue - 0.5);
+            currentValue = Math.max(0, currentValue - 1.0);
         }
 
         valueEl.textContent = currentValue.toFixed(1);
@@ -166,57 +229,3 @@ function handleSecondaryColor(color) {
     document.documentElement.style.setProperty('--secondary-color', color.hex);
     document.getElementById('selectedSecondaryColor').textContent = color.name
 }
-
-/*
-Color definitions are from 
-https://discord.com/channels/890339612441063494/1284530105816711168/1375040718308900875 
-
------- COLOUR DEFINITION ----------- COLOUR HEX ------------- COLOUR NAME ------
-#define COLOR_IUIUIU_ORANGE          lv_color_hex(0xE64829) // Gaggiuino Orange 
-#define COLOR_IUIUIU_BLUE            lv_color_hex(0x023047) // Deep Blue 
-#define COLOR_IUIUIU_RED             lv_color_hex(0xd00000) // True Red 
-#define COLOR_IUIUIU_DIRTY_GREEN     lv_color_hex(0x84a98c) // Dirty Green 
-#define COLOR_IUIUIU_LIGHT_BROWN     lv_color_hex(0xe6ccb2) // Light Brown 
-#define COLOR_IUIUIU_PINKISH         lv_color_hex(0xffb5a7) // Pinkish 
-#define COLOR_IUIUIU_DEEP_PURP       lv_color_hex(0xb5179e) // Deep Purple 
-#define COLOR_ESPRESSO               lv_color_hex(0x4B382A) // Espresso Brown 
-#define COLOR_LATTE                  lv_color_hex(0xC89F91) // Latte Cream 
-#define COLOR_MOCHA                  lv_color_hex(0x3E2723) // Mocha Brown 
-#define COLOR_CARAMEL                lv_color_hex(0xD2691E) // Caramel Drizzle 
-#define COLOR_MACCHIATO              lv_color_hex(0xFFD700) // Macchiato Gold 
-#define COLOR_CAFFE_AU_LAIT          lv_color_hex(0xA67B5B) // Caffé au Lait 
-#define COLOR_CAPPUCCINO             lv_color_hex(0xB5651D) // Cappuccino Foam 
-#define COLOR_AMERICANO              lv_color_hex(0x2F1B0C) // Americano Black 
-#define COLOR_IRISH_COFFEE           lv_color_hex(0x5A3A22) // Irish Coffee 
-#define COLOR_VANILLA_BEAN           lv_color_hex(0xF5E6CC) // Vanilla Bean 
-#define COLOR_ICED_COFFEE            lv_color_hex(0x8B6F47) // Iced Coffee 
-#define COLOR_RISTRETTO              lv_color_hex(0x362511) // Ristretto 
-#define COLOR_LIGHT_BLUE             lv_color_hex(0xADD8E6) // Light Blue 
-#define COLOR_MEDIUM_LIGHT_BLUE      lv_color_hex(0x87CEFA) // Medium Light Blue 
-#define COLOR_DARK_BLUE              lv_color_hex(0x00008B) // Dark Blue 
-#define COLOR_LIGHT_GREEN            lv_color_hex(0x90EE90) // Light Green 
-#define COLOR_MEDIUM_LIGHT_GREEN     lv_color_hex(0x32CD32) // Medium Light Green 
-#define COLOR_DARK_GREEN             lv_color_hex(0x006400) // Dark Green 
-#define COLOR_LIGHT_YELLOW           lv_color_hex(0xFFFFE0) // Light Yellow 
-#define COLOR_MEDIUM_LIGHT_YELLOW    lv_color_hex(0xFFFACD) // Medium Light Yellow 
-#define COLOR_DARK_YELLOW            lv_color_hex(0xFFD700) // Dark Yellow 
-#define COLOR_LIGHT_PURPLE           lv_color_hex(0xE6E6FA) // Light Purple 
-#define COLOR_MEDIUM_LIGHT_PURPLE    lv_color_hex(0x9370DB) // Medium Light Purple 
-#define COLOR_DARK_PURPLE            lv_color_hex(0x4B0082) // Dark Purple 
-#define COLOR_LIGHT_RED              lv_color_hex(0xFFB6C1) // Light Red 
-#define COLOR_MEDIUM_LIGHT_RED       lv_color_hex(0xFF6347) // Medium Light Red 
-#define COLOR_DARK_RED               lv_color_hex(0x8B0000) // Dark Red 
-#define COLOR_LIGHT_BROWN            lv_color_hex(0xD2B48C) // Light Brown 
-#define COLOR_MEDIUM_LIGHT_BROWN     lv_color_hex(0xA0522D) // Medium Light Brown 
-#define COLOR_DARK_BROWN             lv_color_hex(0x654321) // Dark Brown 
-#define COLOR_LIGHT_ORANGE           lv_color_hex(0xFFA07A) // Light Orange 
-#define COLOR_MEDIUM_LIGHT_ORANGE    lv_color_hex(0xFF8C00) // Medium Light Orange 
-#define COLOR_DARK_ORANGE            lv_color_hex(0xFF4500) // Dark Orange 
-#define COLOR_LIGHT_PINK             lv_color_hex(0xFFB6C1) // Light Pink 
-#define COLOR_MEDIUM_LIGHT_PINK      lv_color_hex(0xFF69B4) // Medium Light Pink 
-#define COLOR_DARK_PINK              lv_color_hex(0xC71585) // Dark Pin 
-#define COLOR_IUIUIU_PASTEL_AQUA     lv_color_hex(0x8cc1d6) // Light Aqua 
-#define COLOR_IUIUIU_DARK_TEAL       lv_color_hex(0x073e55) // Dark Teal 
-#define COLOR_IUIUIU_DARK_MAGENTA    lv_color_hex(0x4d192c) // Dark magenta 
-
-*/
